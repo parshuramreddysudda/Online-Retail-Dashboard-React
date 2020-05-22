9c086ce8404c3675018573d9557c66852983e541
parshuramreddysudda <parshuram.sudda@gmail.com>
2020-05-22 15:11:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Paper from '@material-ui/core/Paper';
 import '../src/App.css';
 import Barchart from '../src/components/BarCharts';
-import Donut from  '../src/components/Donut';
+import Donut from '../src/components/Donut';
 import Circle from 'react-circle';
 import { Navbar } from 'react-bootstrap';
 import IconButton from '@material-ui/core/IconButton';
@@ -14,38 +14,35 @@ import axios from 'axios';
 
 export default class Index extends React.Component {
 
- constructor(props) {
-   super(props);
-   
-  
-
- }
- state = {
-  apiResult: []
-}
-componentDidMount(){
-  axios.get(` http://127.0.0.1:4010/data`)
-    .then(res => {
-      const apiResult = res.data;
-      this.setState({ apiResult });
-      // console.log(this.state.apiResult);
-    })
-}
- handleRefresh=()=> {
-  axios.get(` http://127.0.0.1:4010/data`)
-    .then(res => {
-      const apiResult = res.data;
-      this.setState({ apiResult });
-      // console.log(this.state.apiResult);
-    })
-}
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    apiResult: []
+  }
+  componentDidMount() {
+    axios.get(` http://127.0.0.1:4010/data`)
+      .then(res => {
+        const apiResult = res.data;
+        this.setState({ apiResult });
+        // console.log(this.state.apiResult);
+      })
+  }
+  handleRefresh = () => {
+    axios.get(` http://127.0.0.1:4010/data`)
+      .then(res => {
+        const apiResult = res.data;
+        this.setState({ apiResult });
+        // console.log(this.state.apiResult);
+      })
+  }
 
 
 
   render() {
 
-    const rightText={
-      textAlign:'right',
+    const rightText = {
+      textAlign: 'right',
     }
     const paper = {
       padding: '1em',
@@ -68,11 +65,11 @@ componentDidMount(){
       padding: "1em",
       fontSize: "15px",
     }
-    const but={
-      background:'white',
+    const but = {
+      background: 'white',
     }
-    const result=this.state.apiResult;
-    var totalSum=parseInt(result.amazon)+parseInt(result.flip)+parseInt(result.ebay);
+    const result = this.state.apiResult;
+    var totalSum = parseInt(result.amazon) + parseInt(result.flip) + parseInt(result.ebay);
 
 
 
@@ -80,25 +77,25 @@ componentDidMount(){
 
       <div className='darkBack' >
         <Paper style={paper}><h5>Online Retail Dashboard</h5></Paper>
-        
+
         <Navbar style={smal} className='header'>
-  <Navbar.Brand href="#home " className='darkBack'>Summary</Navbar.Brand>
-  <Navbar.Toggle />
-  <Navbar.Collapse className="justify-content-end darkBack">
-    <Navbar.Text className='darkBack' style={{marginRight:'5px'}}>
-      Click to Refresh  
+          <Navbar.Brand href="#home " className='darkBack'>Summary</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end darkBack">
+            <Navbar.Text className='darkBack' style={{ marginRight: '5px' }}>
+              Click to Refresh
     </Navbar.Text>
-    <IconButton aria-label="Change the Values" style={but} onClick={this.handleRefresh} >
-  <RefreshOutlinedIcon></RefreshOutlinedIcon>
-</IconButton> 
-  </Navbar.Collapse>
-</Navbar>
+            <IconButton aria-label="Change the Values" style={but} onClick={this.handleRefresh} >
+              <RefreshOutlinedIcon></RefreshOutlinedIcon>
+            </IconButton>
+          </Navbar.Collapse>
+        </Navbar>
         <div className=' container-fluid pr-5 pl-5 section' style={smal}>
           <div className='row'>
             <div className='col-md-3 col-lg-3'>
               <Paper className="darkCard">
                 <div className='card-title cardTitleDark' style={main}>
-                <p className='compPosition'>Total Revenue  </p><p style={rightText} > <i class="fas fa-money-check-alt"></i></p>
+                  <p className='compPosition'>Total Revenue  </p><p style={rightText} > <i class="fas fa-money-check-alt"></i></p>
                 </div>
                 <div className='card-text' style={prize}>
                   <icon style={{ fontSize: '19px' }}>$</icon> {totalSum}K
@@ -108,7 +105,7 @@ componentDidMount(){
             <div className='col-md-3'>
               <Paper className="darkCard">
                 <div className='card-title cardTitleDark' style={main}>
-                <p className='compPosition'>Revenue from Amazon  </p><p style={rightText} > <i class="fab fa-amazon"></i></p>
+                  <p className='compPosition'>Revenue from Amazon  </p><p style={rightText} > <i class="fab fa-amazon"></i></p>
 
                 </div>
                 <div className='card-text' style={prize}>
@@ -119,7 +116,7 @@ componentDidMount(){
             <div className='col-md-3'>
               <Paper className="darkCard">
                 <div className='card-title cardTitleDark' style={main}>
-                <p className='compPosition'>Revenue from Ebay  </p><p style={rightText} > <i class="fab fa-ebay"></i></p>
+                  <p className='compPosition'>Revenue from Ebay  </p><p style={rightText} > <i class="fab fa-ebay"></i></p>
 
                 </div>
                 <div className='card-text' style={prize}>
@@ -130,7 +127,7 @@ componentDidMount(){
             <div className='col-md-3'>
               <Paper className="darkCard">
                 <div className='card-title cardTitleDark' style={main}>
-                <p className='compPosition'>Revenue from Flikart  </p><p style={rightText} > <i class="fas fa-shopping-cart"></i></p>
+                  <p className='compPosition'>Revenue from Flikart  </p><p style={rightText} > <i class="fas fa-shopping-cart"></i></p>
 
                 </div>
                 <div className='card-text' style={prize}>
@@ -149,7 +146,7 @@ componentDidMount(){
             <div className='col-md-3 col-lg-3 '>
               <Paper className="darkCard fullHeight">
                 <div className='card-title cardTitleDark' style={main}>
-                Product Views
+                  Product Views
                 </div>
                 <br></br>
                 <br></br>
@@ -201,7 +198,7 @@ componentDidMount(){
 
             </div>
             <div className='col-md-6 col-lg-6'>
-            <Paper className="darkCard" style={smal} ><Donut amazon={result.comp1} flip={result.comp2} ebay={result.comp3}  ></Donut></Paper>
+              <Paper className="darkCard" style={smal} ><Donut amazon={result.comp1} flip={result.comp2} ebay={result.comp3}  ></Donut></Paper>
             </div>
           </div>
         </div>
